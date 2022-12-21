03277b81ab9a3cb228f2b4f8938c207b0ab3f9b2
khvgiorgi <gamgichab@gmail.com>
2022-12-22 00:00:22 +0400

    something

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import {Route, Switch} from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import Favorites from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <div></div>;
+    return <div>
+        <MainNavigation />
+        <Switch>
+            <Route path='/' exact>
+                <AllMeetupsPage/>
+            </Route>
+            <Route path='/new-meetup'w>
+                <NewMeetupPage/>
+            </Route>
+            <Route path='/favorites'>
+                <Favorites/>
+            </Route>
+        </Switch>
+    </div>
 }
 
 export default App;
